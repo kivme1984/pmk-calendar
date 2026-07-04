f3c50a823c4412ef3ed0ec859dfecf05419d7715
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 13:35:09 +0300

    Refresh service worker for keyboard done button

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 keyboard-stable-3
+// PMK Calendar v82.20.0 keyboard-done-4
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-keyboard-stable-3');
+importScripts('./sw-core-v82.js?v=82.20.0-keyboard-done-4');
