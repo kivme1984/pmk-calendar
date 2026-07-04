123f64fe5172c856a81e5796dd98c2169ab34433
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 10:39:04 +0300

    Refresh service worker for matched text search display

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 search-key-display-1
+// PMK Calendar v82.20.0 search-source-text-2
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-search-key-display-1');
+importScripts('./sw-core-v82.js?v=82.20.0-search-source-text-2');
