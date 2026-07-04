650ef7487a85af8e8f0e948b5bc7f8dd3c6f8727
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 13:12:43 +0300

    Refresh service worker for mobile keyboard fix

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 search-source-text-2
+// PMK Calendar v82.20.0 keyboard-form-fix-1
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-search-source-text-2');
+importScripts('./sw-core-v82.js?v=82.20.0-keyboard-form-fix-1');
