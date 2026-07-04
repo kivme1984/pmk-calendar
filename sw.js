c479fd9945f3ade584a510287783fcf31ba4cd66
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-05 02:13:01 +0300

    Update sw pointer v82.47

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.46.0
-importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.46.0');
+// PMK Calendar v82.47.0
+self.importScripts('./sw-special-router-v82.js');
+self.importScripts('./sw-core-v82.js?v=82.47.0');
