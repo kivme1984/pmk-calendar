f23e29160d61ec0fd846f3e93a24551f0ac7582e
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 13:53:29 +0300

    Refresh sw for non sticky mobile form actions

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 clean-kbd-7
+// PMK Calendar v82.20.0 no-sticky-form-actions-8
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-clean-kbd-7');
+importScripts('./sw-core-v82.js?v=82.20.0-no-sticky-form-actions-8');
