a7e944e887036e2fecdc1ca14c22b35ce7ee4c64
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-05 00:04:58 +0300

    Point service worker to 82.33 cache

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.32.0
+// PMK Calendar v82.33.0
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.32.0');
+importScripts('./sw-core-v82.js?v=82.33.0');
