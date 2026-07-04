8b6d4638906a212397dca37f3440b6cd978778d2
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 13:43:30 +0300

    Refresh sw for keyboard final button

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 keyboard-done-4
+// PMK Calendar v82.20.0 kbd-end-6
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-keyboard-done-4');
+importScripts('./sw-core-v82.js?v=82.20.0-kbd-end-6');
