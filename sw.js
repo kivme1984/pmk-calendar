38f5671507007b182aa26c2411baf09721692ab4
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 14:07:35 +0300

    Refresh sw for update manager

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 v50-editor-kbd-9
+// PMK Calendar v82.20.0 update-manager-10
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-v50-editor-kbd-9');
+importScripts('./sw-core-v82.js?v=82.20.0-update-manager-10');
