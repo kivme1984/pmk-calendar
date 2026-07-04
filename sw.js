bd172b85619d48b3565e5ef51c068f37e81c1b2f
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 22:31:10 +0300

    Use 82.23 service worker core

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.22.0 fast-address-select
+// PMK Calendar v82.23.0
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.22.0-fast-address-select');
+importScripts('./sw-core-v82.js?v=82.23.0');
