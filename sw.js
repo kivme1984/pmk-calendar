ace34dd601cb983bd7566a88a2a62dd72a5dde4a
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 22:23:27 +0300

    Point service worker to 82.22 cache

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.21.0 address-label-cache
+// PMK Calendar v82.22.0 fast-address-select
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.21.0-address-label-cache');
+importScripts('./sw-core-v82.js?v=82.22.0-fast-address-select');
