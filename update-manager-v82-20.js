3f3090a9609ccd73354fe1d43d024da414db571f
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-05 00:52:32 +0300

    Update manager current version to v82.39

--- update-manager-v82-20.js
+++ update-manager-v82-20.js
@@ -1,14 +1,15 @@
 'use strict';
 
 (() => {
-  if (window.PMK_UPDATE_MANAGER_V82_38) return;
+  if (window.PMK_UPDATE_MANAGER_V82_39) return;
   window.PMK_UPDATE_MANAGER_V82_20 = true;
   window.PMK_UPDATE_MANAGER_V82_35 = true;
   window.PMK_UPDATE_MANAGER_V82_37 = true;
   window.PMK_UPDATE_MANAGER_V82_38 = true;
+  window.PMK_UPDATE_MANAGER_V82_39 = true;
 
-  const CURRENT = '82.38.0';
-  const CURRENT_TOKEN = 'restore-day-card-layout-v82-38';
+  const CURRENT = '82.39.0';
+  const CURRENT_TOKEN = 'disable-left-status-column-v82-39';
   const CHECK_EVERY = 1000 * 60 * 20;
   const STYLE_ID = 'pmkUpdateManagerStyle';
   const INSTALLED_VERSION_KEY = 'pmk-installed-version';
