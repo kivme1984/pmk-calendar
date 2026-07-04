b2acc2ca86b3db3aa1409ca478c6435b30544efa
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 13:49:01 +0300

    Refresh sw for clean keyboard scroll fix

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 kbd-end-6
+// PMK Calendar v82.20.0 clean-kbd-7
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-kbd-end-6');
+importScripts('./sw-core-v82.js?v=82.20.0-clean-kbd-7');
