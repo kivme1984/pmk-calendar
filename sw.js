35713da60ad9ee8a766dcb3e8772a35303dfe273
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-04 14:02:07 +0300

    Refresh sw for v50 editor keyboard fix

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 no-sticky-form-actions-8
+// PMK Calendar v82.20.0 v50-editor-kbd-9
 importScripts('./sw-special-router-v82.js');
-importScripts('./sw-core-v82.js?v=82.20.0-no-sticky-form-actions-8');
+importScripts('./sw-core-v82.js?v=82.20.0-v50-editor-kbd-9');
