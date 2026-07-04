dc2d991b56b20e606fd26b0a57d5e5960204f849
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-05 01:22:35 +0300

    Update manager current version to v82.44

--- update-manager-v82-20.js
+++ update-manager-v82-20.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (() => {
-  if (window.PMK_UPDATE_MANAGER_V82_43) return;
+  if (window.PMK_UPDATE_MANAGER_V82_44) return;
   window.PMK_UPDATE_MANAGER_V82_20 = true;
   window.PMK_UPDATE_MANAGER_V82_35 = true;
   window.PMK_UPDATE_MANAGER_V82_37 = true;
@@ -11,9 +11,10 @@
   window.PMK_UPDATE_MANAGER_V82_41 = true;
   window.PMK_UPDATE_MANAGER_V82_42 = true;
   window.PMK_UPDATE_MANAGER_V82_43 = true;
+  window.PMK_UPDATE_MANAGER_V82_44 = true;
 
-  const CURRENT = '82.43.0';
-  const CURRENT_TOKEN = 'tight-card-narrow-status-buttons-v82-43';
+  const CURRENT = '82.44.0';
+  const CURRENT_TOKEN = 'thin-status-buttons-not-narrow-v82-44';
   const CHECK_EVERY = 1000 * 60 * 20;
   const STYLE_ID = 'pmkUpdateManagerStyle';
   const INSTALLED_VERSION_KEY = 'pmk-installed-version';
