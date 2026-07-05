a040acf7e0aa6c37b9c026fbffb8ff30b9a99917
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-05 10:58:36 +0300

    Use clean 82.48.2 service worker core

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.48.1
+// PMK Calendar v82.48.2
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82.js?v=82.48.1');
+self.importScripts('./sw-core-v82-48-2.js?v=82.48.2');
