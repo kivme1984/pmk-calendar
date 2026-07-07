fea63abbe7dee609a3235a83e647630fff6db938
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 06:38:27 +0300

    Force service worker import to cardfix5 active status colors

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix4 status colors edge indicator
+// PMK Calendar v82.20.0 — cardfix5 active status colors
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix4-status-colors-edge');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix5-active-status-colors');
