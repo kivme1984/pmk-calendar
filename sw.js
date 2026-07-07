bfb3814828cfdefd0264dce2720b0cb024edd635
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 07:54:31 +0300

    Force service worker import to cardfix11 bottom buttons height

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix10 real drafts menu light
+// PMK Calendar v82.20.0 — cardfix11 bottom actions height lock
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix10-real-drafts-menu-light');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix11-bottom-actions-height-lock');
