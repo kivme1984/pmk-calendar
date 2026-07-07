96379bce0a40bf3d41cbca2eb61761547d9879d4
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 08:58:35 +0300

    Update sw to cardfix16 v50 preview rugs and cost

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 - cardfix15 rug number no circle
+// PMK Calendar v82.20.0 - cardfix16 v50 preview rugs and cost
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix15-rug-number-no-circle');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix16-v50-preview-rug-cost');
