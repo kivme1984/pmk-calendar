f16067696adc0d086dd2ebf22bd3542c5d00da71
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 07:00:41 +0300

    Force service worker import to cardfix7 active counters

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix6 stable render no jump
+// PMK Calendar v82.20.0 — cardfix7 no jump active counters
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix6-stable-render-no-jump');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix7-no-jump-active-counters');
