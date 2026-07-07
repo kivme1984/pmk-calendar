28bbf6948754abe35db8c7f0e53dcd8baa53eb65
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 07:47:16 +0300

    Force service worker import to cardfix10 real drafts

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix9 yellow loupe draft button
+// PMK Calendar v82.20.0 — cardfix10 real drafts menu light
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix9-search-loupe-draft');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix10-real-drafts-menu-light');
