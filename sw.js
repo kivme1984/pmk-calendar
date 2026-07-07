7d3c75c2b7c20b0da5a31702529a7faea071663c
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 08:46:54 +0300

    Update sw to cardfix14 form compact

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 - cardfix13 smart paste compact
+// PMK Calendar v82.20.0 - cardfix14 form compact
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix13-smart-paste-compact');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix14-form-compact');
