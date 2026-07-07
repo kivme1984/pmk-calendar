113e3fa0c5d83f1a63f75ed037b426186e2e6349
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 08:52:30 +0300

    Update sw to cardfix15 rug number no circle

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 - cardfix14 form compact
+// PMK Calendar v82.20.0 - cardfix15 rug number no circle
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix14-form-compact');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix15-rug-number-no-circle');
