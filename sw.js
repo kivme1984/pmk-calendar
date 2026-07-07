50d5c9091d656724e26d66afdc6efc0548cbb9d1
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 09:00:06 +0300

    Update sw to cardfix17 preview only revert normal form

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 - cardfix16 v50 preview rugs and cost
+// PMK Calendar v82.20.0 - cardfix17 preview only, normal form restored
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix16-v50-preview-rug-cost');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix17-preview-only-revert-form');
