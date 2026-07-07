ad23d812ea142be4d0e2ba0dfa4f4ec939979e0d
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 07:13:13 +0300

    Force service worker import to cardfix8 header search

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix7 no jump active counters
+// PMK Calendar v82.20.0 — cardfix8 header search
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix7-no-jump-active-counters');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix8-header-search');
