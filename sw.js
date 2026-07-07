bc2478a8e1eb9e0b9406994573ebdf1b37114e29
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 07:34:48 +0300

    Force service worker import to cardfix9 search loupe draft

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix8 header search
+// PMK Calendar v82.20.0 — cardfix9 yellow loupe draft button
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix8-header-search');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix9-search-loupe-draft');
