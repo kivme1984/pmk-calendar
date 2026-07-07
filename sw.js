b841e783c72c0cbb2309abfb3b99100976db0fa2
kivme1984 <105222762+kivme1984@users.noreply.github.com>
2026-07-07 07:57:59 +0300

    Update sw to cardfix12

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// PMK Calendar v82.20.0 — cardfix11 bottom actions height lock
+// PMK Calendar v82.20.0 - cardfix12 draft counter
 self.importScripts('./sw-special-router-v82.js');
-self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix11-bottom-actions-height-lock');
+self.importScripts('./sw-core-v82-48-3.js?v=82.20.0-cardfix12-draft-counter');
